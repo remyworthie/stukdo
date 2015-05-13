2b25ab7a67ed1a53dbfaeef90ff5910e6e2f6918
Remy Worthie <remy.marshall@gmail.com>
2015-05-13 18:28:36 +0000

    added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  
+  get 'about' => "pages#about"
 
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
